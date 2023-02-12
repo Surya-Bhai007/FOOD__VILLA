63b9e5d153b7cda77b8d52ccbc23af128b272acb
Surya-Bhai007 <surajkumar9013078466@gmail.com>
2023-02-12 23:37:56 +0530

    starting tools

---
+++ app.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const header =(
+    <h1>starting tools</h1>
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Initializing Root and render
+const Root = ReactDOM.createRoot(document.getElementById("root"));
+Root.render(header);
